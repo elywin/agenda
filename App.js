c5bcba753a55613ac1d10ba772bd273219446ff0
elywin <lukeeraelywin@gmail.com>
2020-07-16 01:46:41 +0300

    ft(add:created meeting agenda

---
+++ App.js
@@ -0,0 +1,13 @@
+var pad = document.querySelector (".container");
+pad.style.padding="100px";
+
+var title = document.querySelector(".title2");
+title.style.color="blue";
+
+var heading = document.querySelector("h1");
+heading.style.color="blue";
+heading.style.textAlign="center";
+heading.style.fontSize="60px";
+
+var head = document.querySelector("span");
+head.style.fontStyle="normal";
